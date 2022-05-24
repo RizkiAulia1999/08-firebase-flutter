f924e0cd130aa8ce3fc546dd812b07b00a035f3b
aresm1999 <auliarizki0970@gmail.com>
2022-05-24 15:27:49 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_firebase/page/login_page.dart';
 
-void main() => runApp(MyApp());
+//void main() => runApp(MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Login',
+      title: '(Muslimatul Rizi Aulia - 2031710076)',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
---
+++ lib/screen/first_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/page/login_page.dart';
+import 'package:flutter_firebase/helper/sign_in.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("(2031710076) Muslimatul Rizki Aulia")),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color.fromARGB(255, 251, 251, 252),
+              Color.fromARGB(255, 250, 250, 250)
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                  imageUrl!,
+                ),
+                radius: 60,
+                backgroundColor: Colors.transparent,
+              ),
+              SizedBox(height: 40),
+              Text(
+                'NAME',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54),
+              ),
+              Text(
+                name!,
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Color.fromARGB(197, 0, 149, 198),
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'EMAIL',
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54),
+              ),
+              Text(
+                email!,
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Color.fromARGB(197, 0, 149, 198),
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 50),
+              RaisedButton(
+                onPressed: () {
+                  signOutGoogle();
+
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(builder: (context) {
+                    return LoginPage();
+                  }), ModalRoute.withName('/'));
+                },
+                color: Color.fromARGB(197, 0, 149, 198),
+                child: Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Text(
+                    'SIGN OUT',
+                    style: TextStyle(fontSize: 15, color: Colors.white),
+                  ),
+                ),
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
